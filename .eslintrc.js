10ff17648f702eeab74b58c561f10e183a159c51
Stanislav Lebedev <kugui@yandex.ru>
2019-04-27 08:14:00 +0400

    feat(common): flow type linter has been added

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,6 +12,7 @@ module.exports = {
         ecmaVersion: 9,
     },
     plugins: [
+        'flowtype',
         'jest',
         'jsdoc',
         'react',
@@ -20,11 +21,15 @@ module.exports = {
     extends: [
         'airbnb',
         'eslint:recommended',
+        'plugin:flowtype/recommended',
         'plugin:jest/recommended',
         'prettier',
         'prettier/react',
     ],
     settings: {
+        flowtype: {
+            onlyFilesWithFlowAnnotation: true,
+        },
         'import/resolver': {
             webpack: {
                 config: './webpack.config.js',
