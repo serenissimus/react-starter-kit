e25d8b5fdc5a7aea50db7ee52436ba73824ee99c
Stanislav Lebedev <kugui@yandex.ru>
2019-05-06 20:59:14 +0400

    fix: jest configuration fixed

--- jest.config.js
+++ jest.config.js
@@ -1,8 +1,11 @@
 module.exports = {
-    rootDir: './tests',
+    rootDir: '.',
+    moduleFileExtensions: ['js', 'jsx'],
+    moduleDirectories: ['src', 'node_modules'],
     moduleNameMapper: {
         '^.+\\.(css|less)$': 'identity-obj-proxy',
-        '^.+\\.(jpg|jpeg|png)$': 'identity-obj-proxy',
+        '^.+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$':
+            '<rootDir>/jest/mocks/file.js',
     },
     testMatch: ['**/?(*.)(spec).(js|jsx)'],
 };
--- tests/App.spec.jsx
+++ tests/App.spec.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
-import App from '../src/components/App';
+import App from 'components/App';
 
 describe('App', () => {
-    const props = {
-        title: 'Title',
-        logo: <div />,
-    };
-
     it('should correct render', () => {
+        const component = renderer.create(<App />);
+        const tree = component.toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should correct render with props', () => {
+        const props = {
+            title: 'Title',
+            logo: 'logo',
+        };
+
         const component = renderer.create(<App {...props} />);
         const tree = component.toJSON();
         expect(tree).toMatchSnapshot();
